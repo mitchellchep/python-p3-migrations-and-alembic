b9d37c6bd201d0a9ee72f2da0e8ff33f01bb66fe
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-12-17 22:36:19 +0300

    Lab done
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

---
+++ app/migrations/versions/66d59b85e617_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 66d59b85e617
+Revises: 
+Create Date: 2023-12-06 15:26:41.711184
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '66d59b85e617'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
